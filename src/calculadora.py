4eed72e3ce0f53a34d93c7fa9171e68d0b632f9e
Michel Albonico <bsd.albonico@gmail.com>
2025-06-16 19:58:53 -0300

    Tests

---
+++ src/calculadora.py
@@ -0,0 +1,23 @@
+class Calculadora:
+
+    def soma(self, num1, num2):
+        return num1 + num2
+
+    def subtracao(self, num1, num2):
+        return num1 - num2
+
+    def multiplicacao(self, num1, num2):
+        return num1 * num2
+
+    def divisao(self, num1, num2):
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Erro: divisão por zero"
+
+def main():
+    calc = Calculadora()
+    print(calc.soma(2, 2))
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_calc.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from calculadora import Calculadora
+
+def test_soma_1():
+    calc = Calculadora()
+    assert calc.soma(2,2) == 4
+
+def test_soma():
+    calc = Calculadora()
+    assert calc.soma(2,3) == 5
+
+def test_sub():
+    calc = Calculadora()
+    assert calc.subtracao(2,2) == 0
+
+def test_div():
+    calc = Calculadora()
+    assert calc.divisao(2,1) == 2
